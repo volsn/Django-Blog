0796d92bfa8000aaa47d88bbb4131c8eab318883
swMDAbBv <mpxCQEsCd8>
2021-04-15 21:22:45 +0300

    Trying to turn the debug mode on

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
