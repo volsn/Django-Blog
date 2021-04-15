44bb690c30e73a1981a9fa95971c737d9331865b
swMDAbBv <mpxCQEsCd8>
2021-04-15 21:04:16 +0300

    Started working on the admin panel

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from miloblog import app
+from miloblog import app, db
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from miloblog.models import BlogPost, User, Comment, NewsletterSubscription
+
+admin = Admin(app)
+admin.add_view(ModelView(BlogPost, db.session))
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Comment, db.session))
+admin.add_view(ModelView(NewsletterSubscription, db.session))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
