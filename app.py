d089f4c122dc87e1940743cfaaebc1bbbc11690c
swMDAbBv <mpxCQEsCd8>
2021-04-15 21:14:16 +0300

    Added logging

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 from miloblog import app, db
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -8,6 +9,9 @@ admin.add_view(ModelView(BlogPost, db.session))
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Comment, db.session))
 admin.add_view(ModelView(NewsletterSubscription, db.session))
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run(debug=True)
